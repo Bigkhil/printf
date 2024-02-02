2453fe6c9d50dfc8f2b88a8efa4cb5b8d3824a12
Mohamed khalil <khm40005000@outlook.com>
2024-02-02 15:40:53 +0200

    some changes

--- _printf.c
+++ _printf.c
@@ -1,4 +1,23 @@
 #include "main.h"
+/**
+ * printstringspecifier - function to print string
+ * @p: pointer to char
+ * Return: pointer to char
+ */
+char *printstringspecifier(char *p)
+{
+	while (*p != '\0')
+	{
+		write(1, p, 1);
+		p++;
+	}
+	return (p);
+}
+/**
+ * printchar - function to print string
+ * @ch: char
+ * Return: void
+ */
 void printchar(int ch)
 {
 	write(1, &ch, 1);
@@ -25,12 +44,14 @@ const char *printstr(const char *str)
 int _printf(const char *format, ...)
 {
 	va_list ap;
+	char *stringstart, *stringend;
 	int cntr;
-	const char *x;
+	const char *x, *temp;
 
 	va_start(ap, format);
 	cntr = 0;
 	x = printstr(format);
+	cntr += (x - format);
 	while (*x != '\0')
 	{
 		x++;
@@ -38,13 +59,23 @@ int _printf(const char *format, ...)
 		{
 			case ('c'):
 			printchar(va_arg(ap, int));
+			cntr++;
 			break;
 			case ('%'):
 			write(1, x, 1);
+			cntr++;
 			break;
 			case ('s'):
-			
+			stringstart = va_arg(ap, char *);
+			stringend = printstringspecifier(stringstart);
+			cntr += (stringend - stringstart);
+			break;
 		}
+		x++;
+		temp = printstr(x);
+		cntr += (temp - x);
+		x = temp;
 	}
-	cntr += (x - format);
+	va_end(ap);
+	return (cntr);
 }
--- test.c
+++ test.c
@@ -10,8 +10,15 @@
 int main(void)
 {
 	int len;
+	char x;
+	char *name = "mohamed";
 
+	x = 'A';
 	len = _printf("Let's try to printf a simple sentence.\n");
-	printf("%d", len);
+	printf("%d\n", len);
+	len = _printf("my name is %s i have %c years old\n", name, x);
+	printf("%d\n", len);
+	len = _printf("this is a percentage %%\n");
+	printf("%d\n", len);
 	return (0);
 }
